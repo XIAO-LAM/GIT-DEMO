b958f121ba61d17b149f980712d7b7fe9a8ea844
xiao <2625891152@qq.com>
2016-03-05 17:18:36 +0800

    Add DB.java

---
+++ src/com/example/gitdemo/DB.java
@@ -0,0 +1,19 @@
+/*
+ * @Title:  DB.java
+ * @Copyright:  XXX Co., Ltd. Copyright YYYY-YYYY,  All rights reserved
+ * @Description:  TODO<请描述此文件是做什么的>
+ * @author:  XIAO
+ * @data:  2016-3-5 下午5:17:40
+ * @version:  V1.0
+ */
+package com.example.gitdemo;
+
+/**
+ * TODO<请描述这个类是干什么的>
+ * @author  XIAO
+ * @data:  2016-3-5 下午5:17:40
+ * @version:  V1.0
+ */
+public class DB {
+
+}
